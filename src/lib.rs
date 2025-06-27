2ad2f1ed5a8b82ca21eb31daa6db2221081695f1
Sewer56 <sewer56lol@googlemail.com>
2025-06-27 17:31:13 +0100

    Added: Attribute to enable naked function

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 #![cfg_attr(feature = "estimator-avx512", feature(stdarch_x86_avx512))]
 #![cfg_attr(feature = "estimator-avx512", feature(avx512_target_feature))]
 #![allow(stable_features)]
+#![cfg_attr(
+    all(target_arch = "x86", feature = "nightly"),
+    feature(naked_functions_target_feature)
+)]
 
 #[cfg(feature = "c-exports")]
 pub mod exports;
